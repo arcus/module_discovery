2bde80d4a3b88e5bccf919e59c44cda4a335bc94
DRELLICHE <drelliche@chop.edu>
2023-09-01 13:09:04 -0400

    add edges

--- module_network_analysis.py
+++ module_network_analysis.py
@@ -1,6 +1,7 @@
 ## Use networkx tools to analyze the network proprities of the graph of modules so that that data can be incorporated into later visualizations.
 
 import networkx as nx 
+import matplotlib.pyplot as plt
 
 # Import the module data as a dataframe
 import module_data
@@ -15,7 +16,16 @@ for module_id in df.index:
     for linked_module in df.index:
         if str(linked_module) in str(df.loc[module_id, "sets_you_up_for"]):
             G.add_edge(module_id, linked_module)
-        #elif str(linked_module) in str(df.loc[module_id, "depends_on_knowledge_available_in"]):
-        #    G.add_edge(linked_module, module_id)
+        elif str(linked_module) in str(df.loc[module_id, "depends_on_knowledge_available_in"]):
+            G.add_edge(linked_module, module_id)
+        elif str(linked_module) in str(df.loc[module_id, "Linked Courses"]):
+            G.add_edge(linked_module, module_id)
 
 print(G)
+# nx.draw_shell(G)
+# pos = nx.spring_layout(G, iterations=100)
+# nx.draw_networkx(G,pos=pos)
+# plt.show()
+print(len(list(nx.connected_components(nx.Graph(G)))))
+
+print(type(nx.adjacency_matrix(G)))
