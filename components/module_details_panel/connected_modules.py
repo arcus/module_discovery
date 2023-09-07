01b10023126a7a070198bf566bb9cdc2cf5a1261
DRELLICHE <drelliche@chop.edu>
2023-09-07 13:53:31 -0400

    fix depends_on_knowledge_available_in variable

--- components/module_details_panel/connected_modules.py
+++ components/module_details_panel/connected_modules.py
@@ -13,14 +13,14 @@ def connected_modules(active_node):
         sets_you_up_button_list = []
         sets_you_up_for = str(module_data.df.loc[active_node, "sets_you_up_for"])
         depends_on_button_list = []
-        depends_on_knowledge_in = str(module_data.df.loc[active_node, "depends_on_knowledge_in"])
+        depends_on_knowledge_available_in = str(module_data.df.loc[active_node, "depends_on_knowledge_available_in"])
         hidden_button_list = []
         ## add an is_parallel_to optional set of buttons when that metadata is in module_data
         for module in list(module_data.df.index):
-            if module in sets_you_up_for or active_node in str(module_data.df.loc[module,"depends_on_knowledge_in"]): #ensure that if the link is only in one of the modules, it still shows up here (symmetry in metadata not required)
+            if module in sets_you_up_for or active_node in str(module_data.df.loc[module,"depends_on_knowledge_available_in"]): #ensure that if the link is only in one of the modules, it still shows up here (symmetry in metadata not required)
                 button = html.Button(module_data.df.loc[module,"title"], id=module+"_nottub", n_clicks=0) #this is to ensure the buttons created here don't clash with buttons created by filtering, but are still essentially called the same thing for my human brain: module_id_button/module_id_nottub
                 sets_you_up_button_list.append(button)
-            elif module in depends_on_knowledge_in or active_node in str(module_data.df.loc[module,"sets_you_up_for"]):
+            elif module in depends_on_knowledge_available_in or active_node in str(module_data.df.loc[module,"sets_you_up_for"]):
                 button = html.Button(module_data.df.loc[module,"title"], id=module+"_nottub", n_clicks=0) #same button/nottub idea here
                 depends_on_button_list.append(button)
             else:
--- module_network_analysis.py
+++ module_network_analysis.py
@@ -11,21 +11,21 @@ df = module_data.df
 G = nx.DiGraph()
 G.add_nodes_from(df.index)
 
-# Add directed edges from depends_on_knowledge_in and sets_you_up_for
+# Add directed edges from depends_on_knowledge_available_in and sets_you_up_for
 for module_id in df.index:
     for linked_module in df.index:
         if str(linked_module) in str(df.loc[module_id, "sets_you_up_for"]):
             G.add_edge(module_id, linked_module)
         elif str(linked_module) in str(df.loc[module_id, "depends_on_knowledge_available_in"]):
             G.add_edge(linked_module, module_id)
-        elif str(linked_module) in str(df.loc[module_id, "Linked Courses"]):
-            G.add_edge(linked_module, module_id)
+        #elif str(linked_module) in str(df.loc[module_id, "Linked Courses"]):
+        #    G.add_edge(linked_module, module_id)
 
 print(G)
-# nx.draw_shell(G)
-# pos = nx.spring_layout(G, iterations=100)
-# nx.draw_networkx(G,pos=pos)
-# plt.show()
+nx.draw_shell(G)
+pos = nx.spring_layout(G, iterations=100)
+nx.draw_networkx(G,pos=pos)
+plt.show()
 print(len(list(nx.connected_components(nx.Graph(G)))))
 
 print(type(nx.adjacency_matrix(G)))
