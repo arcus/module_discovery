e00b2b6229ab59c8f049071791e6e67d964e8d32
DRELLICHE <drelliche@chop.edu>
2023-09-22 10:48:37 -0400

    implement my_modules as a dictionary

--- callbacks/my_modules_in.py
+++ callbacks/my_modules_in.py
@@ -2,21 +2,17 @@
 from dash import Dash, html, Input, Output, dcc, ctx, State
 import dash_bootstrap_components as dbc
 import module_data 
+import ast #This allows the easy conversion from string back to dictionary
 
 ### If any add_to_my_modules buttons are pressed, that module should be added to the list of my_modules
 def my_modules_in(app):
     @app.callback(Output('hidden_my_modules', 'children'),
-                #Input("hidden_my_modules", 'children'),
+                State("hidden_my_modules", 'children'),
                 [Input("add_to_my_modules_"+module_id, 'n_clicks') for module_id in module_data.df.index], #these buttons are add_to_my_modules buttons shown on the module details panel
-                #[Input("remove_from_my_modules"+module_id, 'n_clicks') for module_id in module_data.df.index], #these buttons are remove from my modules buttons shown on the module details panel
                 prevent_initial_call=False)
-    def activate(*args):
-        trigger = ctx.triggered_id
-        return "no_module_in_list ", trigger, sum(args)
-        # if trigger == "module_visualization":
-        #     return data['id']
-        # if sum(args) != 0:
-        #       return trigger[16:]
-        # else:
-        #     return "no_module_in_list "#, trigger, sum(args)
+    def activate(my_modules_dict,*args):
+        trigger = ctx.triggered_id[18:]
+        update_dict = ast.literal_eval(my_modules_dict)
+        update_dict[trigger] = 1
+        return str(update_dict)
 
--- components/hidden_my_modules.py
+++ components/hidden_my_modules.py
@@ -1,8 +1,11 @@
 from dash import Dash, html, Input, Output, dcc, ctx, State
 import dash_bootstrap_components as dbc
+import module_data
+
+my_module_dictionary = {id: 0 for id in module_data.df.index}
 
 hidden_my_modules = [#dcc.Markdown("current active node"),
-    html.Div(children=["dummy"], 
+    html.Div(children= str(my_module_dictionary), 
     id = 'hidden_my_modules', 
     style= {'display': 'block'} # make this 'none' to hide it for final version, 'block' shows this data on the app
     )]
