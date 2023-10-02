269fbbe5425930383daea128cce54bbbc3dbaa6b
DRELLICHE <drelliche@chop.edu>
2023-10-02 16:49:09 -0400

    add "remove all from my list" button

--- callbacks/my_modules_in.py
+++ callbacks/my_modules_in.py
@@ -9,16 +9,23 @@ def my_modules_in(app):
     @app.callback(Output('hidden_my_modules', 'children'),
                 State("hidden_my_modules", 'children'),
                 Input("add_filtered_to_my_modules", 'n_clicks'),
+                Input("remove_filtered_from_my_modules", 'n_clicks'),
                 State("hidden_filtered_modules_list", 'children'),
                 [Input("add_to_my_modules_"+module_id, 'n_clicks') for module_id in module_data.df.index], #these buttons are add_to_my_modules buttons shown on the module details panel
                 [Input("remove_my_modules_"+module_id, 'n_clicks') for module_id in module_data.df.index], #these buttons are remove_my_modules buttons shown on the module details panel
                 prevent_initial_call=True)
-    def activate(my_modules_dict,add_filtered_to_my_modules,hidden_filtered_modules_list,*args):
+    def activate(my_modules_dict,add_filtered_to_my_modules,remove_filtered_from_my_modules,hidden_filtered_modules_list,*args):
 
         if ctx.triggered_id == "add_filtered_to_my_modules":
             update_dict = ast.literal_eval(my_modules_dict)
             for module in list(hidden_filtered_modules_list):
                 update_dict[str(module)] = 1
+            return str(update_dict)
+        
+        elif ctx.triggered_id == "remove_filtered_from_my_modules":
+            update_dict = ast.literal_eval(my_modules_dict)
+            for module in list(hidden_filtered_modules_list):
+                update_dict[str(module)] = 0
             return str(update_dict)
 
         elif ctx.triggered[0]['value'] and ctx.triggered[0]['value'] > 0:
--- components/clickable_module_list/clickable_module_list.py
+++ components/clickable_module_list/clickable_module_list.py
@@ -5,13 +5,16 @@ import module_data
 
 add_all_to_my_modules = dbc.Button("Add all to my list", color="primary", id="add_filtered_to_my_modules")
 
+remove_all_from_my_modules = dbc.Button("Remove all from my list", color="primary", id="remove_filtered_from_my_modules")
+
+
 clickable_module_list = html.Div(
                   [dbc.Col(
                     children=[
                      html.Div([
                         dbc.Row([
                         dbc.Col(dcc.Markdown("Modules that match your filters are listed here and visible in the graph to the right.", style={'background-color': '#ADD8E6'}), width=9),
-                        dbc.Col(add_all_to_my_modules, width=3)
+                        dbc.Col([add_all_to_my_modules,remove_all_from_my_modules], width=3)
                     ])]),
                      html.Div([], id='clickable_module_links')
                      ]
