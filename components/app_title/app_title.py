5510d749980e8e53c1f8d920587ef1eada977b48
DRELLICHE <drelliche@chop.edu>
2023-10-23 16:58:00 -0400

    add research institute logo

--- components/app_title/app_title.py
+++ components/app_title/app_title.py
@@ -1,4 +1,14 @@
 from dash import Dash, html, dcc
 import dash_bootstrap_components as dbc
+import base64
 
-app_title = dbc.Col(html.B(["Module Discovery Tool"]), style={'textAlign': 'center','font-size':'40px'}, align='end')
+image_path = 'assets/RI_logo.png'
+
+# Using base64 encoding and decoding
+def b64_image(image_filename):
+    with open(image_filename, 'rb') as f:
+      image = f.read()
+    return 'data:image/png;base64,' + base64.b64encode(image).decode('utf-8')
+
+
+app_title = dbc.Row([dbc.Col(html.Img(src=b64_image(image_path), style={'height':'90%', 'width':'90%'}), width =2), dbc.Col(html.B(["Module Discovery"]), style={'textAlign': 'center','font-size':'40px'}, align='end'), dbc.Col(html.Img(src=b64_image(image_path), style={'height':'90%', 'width':'90%'}), width =2)])
