2b604b689e54b269a436cb23a855225de82621bb
DRELLICHE <drelliche@chop.edu>
2023-10-25 14:48:54 -0400

    add feedback button and link to REDCap survey

--- components/app_title/app_title.py
+++ components/app_title/app_title.py
@@ -10,5 +10,26 @@ def b64_image(image_filename):
       image = f.read()
     return 'data:image/png;base64,' + base64.b64encode(image).decode('utf-8')
 
+branding_logo = html.A(
+    href="https://www.research.chop.edu/department-of-biomedical-and-health-informatics",
+    children=[
+        html.Img(
+            alt="Link to Children's Hospital of Philadelphia Department of Biomedical and Health Informatics",
+            src=b64_image(image_path),
+            style={'width':'90%'}
+        )
+    ],
+    target="_blank"
+)
 
-app_title = dbc.Row([dbc.Col(html.Img(src=b64_image(image_path), style={'height':'90%', 'width':'90%'}), width =2), dbc.Col(html.B(["Module Discovery"]), style={'textAlign': 'center','font-size':'40px'}, align='end'), dbc.Col(html.Img(src=b64_image(image_path), style={'height':'90%', 'width':'90%'}), width =2)])
+feedback_button = dbc.Button(
+    "Give feedback on this prototype", 
+    color="light gray", 
+    n_clicks=0, 
+    id="feedback_survey",
+    href="https://redcap.link/module_discovery_app_feedback",
+    external_link=True,
+    target="_blank"
+    )
+
+app_title = dbc.Row([dbc.Col(branding_logo, width =2), dbc.Col(html.B(["Module Discovery Tool Prototype"]), style={'textAlign': 'center','font-size':'40px'}, align='end'), dbc.Col(feedback_button, width =2)])
