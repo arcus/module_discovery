b0ae253a42ece12a7f58929062edce32028311fc
DRELLICHE <drelliche@chop.edu>
2023-12-07 16:53:58 -0500

    handle estimated time in minutes as 2 digit string

--- module_data.py
+++ module_data.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 #EDUCATION_MODULES
 
-#read in metadata from education_modules repository, at some point this should be coming from a local copy of the json file, once it works I'll get the action set up.
+#read in metadata from education_modules repository, which is automatically updated each week via a github action
 education_modules_df = pd.read_json("assets/education_modules.json") 
 
 #remove empty rows created by data processing
@@ -11,4 +11,7 @@ education_modules_df= education_modules_df.dropna(axis=0, how='all')
 #set 'module_id' as the index
 education_modules_df = education_modules_df.set_index('module_id')
 
-df=education_modules_df.copy()
+# make estimated_time_in_minutes an integer instead of a float
+education_modules_df['estimated_time_in_minutes'] = education_modules_df['estimated_time_in_minutes'].astype(int)
+
+df=education_modules_df.astype('str').copy()
