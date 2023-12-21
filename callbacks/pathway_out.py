85e24153cd6b6e9c7b6cf5ab4f82c851e8c20670
DRELLICHE <drelliche@chop.edu>
2023-12-21 12:50:11 -0500

    visualize pathway subgraph

--- callbacks/pathway_out.py
+++ callbacks/pathway_out.py
@@ -9,23 +9,34 @@ from network_analysis import poset_processing as poset
 df = module_data.df
 
 def show_pathway_visually(app):
-    @app.callback(Output('testing', 'children'),
+    @app.callback(Output('pathway_visualization', 'elements'),
+                Output('pathway_visualization', 'stylesheet'),
                 Input('hidden_pathway','children'),
+                State('hidden_active_module', 'children'),
                 prevent_initial_call=True) 
-    def update_pathway_graph(hidden_pathway):
-        # Set up the subgraph created by the pathway
+    def update_pathway_graph(hidden_pathway, active_node):
+        ### Set up the subgraph created by the pathway
         pathway_subgraph = poset.G.subgraph(hidden_pathway)
         # Define the graph nodes
         nodes = [{'data': {'id': vertex, 'title': df.loc[vertex,'title'] }} for vertex in pathway_subgraph.nodes()]
         # Define the graph edges
         edges = [{'data': {'source': edge[1], 'target': edge[0]}} for edge in pathway_subgraph.edges()]
-        pathway = cyto.Cytoscape(
-                        id='pathway_visualization',
-                        layout={'name': 'cose'},
-                        elements=edges+nodes,
-                        stylesheet=default_stylesheet,
-                        #style={'width': '100%', 'height':'450px%'},
-                        userZoomingEnabled=False
-                        )
+        # The elements of the graph display are the nodes and the edges:
+        elements=nodes+edges
         
-        return str(pathway) #pathway
+        ### Create a new stylesheet for this induced pathway subgraph
+        # Edges are still neutral
+        new_stylesheet = [ {'selector': 'edge', 'style': default_stylesheet.neutral_edge_styling}]
+        
+        # Node styling is that all of them are selected GET UX HELP TO FIGURE OUT IF THIS MAKES SENSE
+        for module in nodes:
+            module_id = module['data']['id']
+            selector = str('[id *= "')+str(module_id)+str('" ]')
+            # the active node should be distinguishable
+            if module_id == active_node:
+                new_stylesheet +=[{'selector': selector, 'style': default_stylesheet.active_node_styling}]
+            # all nodes in the pathway should be labeled
+            else:            
+                new_stylesheet += [{'selector': selector, 'style': default_stylesheet.selected_styling}]
+
+        return elements, new_stylesheet
