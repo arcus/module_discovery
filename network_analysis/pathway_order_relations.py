54a6db3690e458fb321663c2a4782a7a5a93cfeb
DRELLICHE <drelliche@chop.edu>
2024-01-04 11:39:36 -0500

    Create pathway_order_relations.py

---
+++ network_analysis/pathway_order_relations.py
@@ -0,0 +1,28 @@
+import networkx as nx
+import pandas as pd
+import module_data
+
+import .poset_processing
+
+poset = poset_processing.poset
+hasse = poset_processing.hasse
+
+def prereqs_precede(pathway, module_id):
+    if set(hasse.predecessors(module_id)).issubset(set(pathway[0:pathway.index(module_id)])):
+        return True
+    else:
+        return False
+
+def prereqs_follow(pathway, module_id):
+    all_prereqs = set(poset.predecessors(module_id))
+    if all_prereqs.intersetction(set(pathway[pathway.index(module_id)])) == set():
+        return True
+    else:
+        return False
+
+def prereqs_absent(pathway, module_id):
+    immediate_prereqs = set(hasse.predecessors(module_id)).issubset(set(pathway[0:pathway.index(module_id)]))
+    if immediate_prereqs.issubset(set(pathway)):
+        return False
+    else:
+        return True
