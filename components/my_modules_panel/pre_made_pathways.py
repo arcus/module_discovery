13e65d247e52aa500887dac4b8dabb95acc4a97b
drelliche <99294374+drelliche@users.noreply.github.com>
2024-02-15 14:40:07 -0500

    Update pre_made_pathways.py

--- components/my_modules_panel/pre_made_pathways.py
+++ components/my_modules_panel/pre_made_pathways.py
@@ -5,7 +5,7 @@ big_button_style = {"height":"auto",
                     "width":'18%'}
 
 pre_made_pathways = dbc.Row([
-    html.Button("Getting Started with Biomedical Data Science", style=big_button_style, id="pathway_1"),
+    html.Button("Get Started with Data Science in Biomedicine", style=big_button_style, id="pathway_1"),
     html.Button("Focus on Omics", style=big_button_style, id="pathway_2"),
     html.Button("Big Data, Big Questions", style=big_button_style, id="pathway_3"),
     html.Button("Analysis in R", style=big_button_style, id="pathway_4"),
