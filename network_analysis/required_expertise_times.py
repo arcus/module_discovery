647deefa9c5299b2fc40a8df058733b95a4b3e1b
drelliche <99294374+drelliche@users.noreply.github.com>
2024-05-16 16:36:01 -0400

    Update required_expertise_times.py

--- network_analysis/required_expertise_times.py
+++ network_analysis/required_expertise_times.py
@@ -7,7 +7,7 @@ df = module_data.df
 from .poset_processing import poset, hasse
 
 def required_expertise_times(module_id):
-    pre_req_time = int(0)
-    for module in set(poset.predecessors(module_id)):
-        pre_req_time += int(df.loc[module_id, 'estimated_time_in_minutes'])
+    pre_req_time = 0
+    for module in set(poset.reverse().neighbors(module_id)):
+        pre_req_time += int(df.loc[module, 'estimated_time_in_minutes'])
     return pre_req_time
