be8f859b3c909a64c015f61512c84ca3850fa67b
drelliche <99294374+drelliche@users.noreply.github.com>
2024-07-25 13:56:16 -0400

    Delete hidden_active_module.py

--- components/hidden_active_module.py
+++
@@ -1,8 +0,0 @@
-from dash import Dash, html, Input, Output, dcc, ctx, State
-import dash_bootstrap_components as dbc
-
-hidden_active_module = [#dcc.Markdown("current active node"),
-    html.Div("reproducibility", 
-    id = 'hidden_active_module', 
-    style= {'display': 'none'} # make this 'none' to hide it for final version, 'block' shows this data on the app
-    )]
