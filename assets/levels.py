e56b4be80af8e2c4892ff0f470b725c36973a391
drelliche <99294374+drelliche@users.noreply.github.com>
2024-08-09 10:51:43 -0400

    create level dictionary

---
+++ assets/levels.py
@@ -0,0 +1,30 @@
+from dash import Dash, html, Input, Output, dcc, ctx, State
+import dash_bootstrap_components as dbc
+import module_data 
+import assets.CHOP_colors as CHOP
+from network_analysis.required_expertise_times import required_expertise_times
+
+level_dictionary = {0: {"level_name": "Intro",
+                        "level_description": "No prerequisites",
+                        },
+                    1: {"level_name": "Level 1",
+                        "level_description": "Less than 2 hours of prerequisites"
+                        },
+                    2: {"level_name": "Level 2",
+                        "level_description": "2 to 4 hours of prerequisites"
+                        },
+                    3: {"level_name": "Level 3",
+                        "level_description": "4 to 6 hours of prerequisites"
+                        },
+                    4: {"level_name": "Level 4",
+                        "level_description": "6 to 8 hours of prerequisites"
+                        },
+                    5: {"level_name": "Level 5",
+                        "level_description": "More than 8 hours of prerequisites"
+                        },
+                    }
+
+def module_level(module_id):
+    return (required_expertise_times(module_id)-1)//120 + 1
+
+
